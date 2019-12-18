6a09d893469e573051f3a2383c915b922566abba
huntike <logan.lelay@orange.fr>
2019-12-18 15:29:58 +0100

    add scene loader

---
+++ Assets/Scripts/menu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class menu : MonoBehaviour
+{
+    public string SceneName;
+  
+    public void sceneLoader()
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+
+
+}
